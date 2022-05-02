e58f2fdb839be83cf5a71515aaef023315adb207
DiegoAndP <diegoandrade.xb@gmail.com>
2022-05-02 02:07:08 -0300

    Adicionada lógica do menu de adição de produtos e função localStorage

--- assets/js/adicionar.js
+++ assets/js/adicionar.js
@@ -1,16 +1,52 @@
 const nomeLogin = document.querySelector('.topo__botao');
 
+
 window.addEventListener('load', (e) => {
    e.target.querySelector('.topo__botao').textContent = 'Administrador';
 });
 
-const formulario = document.querySelector('[data-imagem]');
-const formularioWrapper = formulario.closest('.adiciona__wrapper');
 
-formulario.addEventListener('change',(ev)=>{
-   console.log(ev.target);
+const formulario = document.querySelector('.adiciona__form');
+const botaoAdicionaProduto = formulario.querySelector('[data-submit]')
+
+botaoAdicionaProduto.addEventListener('click',(element)=> {
+   element.preventDefault();
+
+   const checaValidity = [];
+   const formulario = document.querySelector('.adiciona__form');
+   const inputs = formulario.querySelectorAll('input');
+   const textArea = formulario.querySelector('textarea').value;
+   const botaoAdicionaProduto = formulario.querySelector('[data-submit]')
+
+   inputs.forEach(el => checaValidity.push(el.validity.valid));
+   if(checaValidity.every(valid => valid == true)) {
+      
+      guardaProduto(inputs, textArea);
+
+
+   }
+     
 });
 
-function adicionaProduto (evento, file) {
+function guardaProduto (inputs, textArea) {
+
+   const textoAlt = "Descrição ainda precisa ser adicionada.";
+
+   if(!textArea) {
+      textArea = textoAlt;
+    }
+
+   const produto = {
+      imagem: inputs[0].nextElementSibling.style.backgroundImage,
+      produto: inputs[1].value,
+      valor: inputs[2].value,
+      categoria: inputs[3].value,
+      descricao : textArea
+   }
+
+   const listaProduto = JSON.parse(localStorage.getItem('produto')) || [];
+   listaProduto.push(produto);   
+   localStorage.setItem('produto', JSON.stringify(listaProduto));
+   window.location.href ="./sucesso.html";
 
 }
--- assets/js/produtos.js
+++
@@ -1,8 +0,0 @@
-function Produto (nome, valor, descricao, imagem, categoria) {
-    this.nome = nome;
-    this.valor = valor;
-    this.descricao = descricao;
-    this.imagem = imagem;
-    this.categoria = categoria;
-}
-
